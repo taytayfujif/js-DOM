867c010b7323224bd21358bd04d3d5d8bc370993
Natalie Ramirez <nramirez787@gmail.com>
2017-07-13 12:17:01 -1000

    updated instructions

--- js/app.js
+++ js/app.js
@@ -2,42 +2,42 @@
 
 /*1. Taylor Swift
 
-Find the div with the id of "name1" and replace the n/a with the following:
+Find the div with the id of "name1" and replace the content with the following:
 
 Tay-Tay*/
 
 
 /*2. DJ Khaled
 
-Find the div with the id of "position2" and replace the n/a with the following: 
-
-Project Manager*/
+Find the div with the id of "position2" and replace the content with the following: Project Manager
+Change the background color to red*/
 
 
 /*3. Piko Taro
 
-Find the div with the id of "alias3" and replace the n/a with the following:
-
-Concatenation*/
+Find the div with the id of "alias3" and replace the content with the following:Concatenation
+Change the font size to 40*/
 
 
 
 
 /*4. Prince
 
-Find the div with the class name of "profile" and replace the n/a with a verse from your favorite Prince song.*/
+Find the div with the class name of "profile" and replace the content with a verse from your favorite Prince song.
+Change the font-family to Sans Serif*/
 
 
 
 /*5. Bruce Lee
 
-Find the div with the class name of "profile" and replace the n/a with a quote from the legend himself.*/
+Find the div with the class name of "profile" and replace the content with a quote from the legend himself.
+*/
 
 
 
 /*6. Samuel L Jackson
 
-Find the div with the class name of "alias" and replace the n/a with your favorite character that Sammy portrayed.*/
+Find the div with the class name of "alias" and replace the content with your favorite character that Sammy portrayed.*/
 
 
 
